a795b284c41656dc99252168ef2e53f0e447e978
Justine Winnie <justine.winnie@gettyimages.com>
2018-03-21 10:19:59 -0700

    Rename Table component, view dummy seed data

--- cool-products/src/App.js
+++ cool-products/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import List from './components/List';
+import Table from './components/Table';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Cool Products List</h1>
+          <h1 className="App-title">Cool Products</h1>
         </header>
-        <List />
+        <Table />
       </div>
     );
   }
---
+++ cool-products/src/components/Table.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import './Table.css';
+
+class Table extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: []
+    };
+  }
+  
+  componentDidMount() {
+    axios.get('http://localhost:3001/products/index.json')
+    .then(response => {
+      this.setState({products: response.data})
+    })
+  }
+  
+  render() {
+    return (
+      <div className="wrapper">
+        <table className="Table">
+          <thead></thead>
+          <tbody>
+            {this.state.products.map((product) => 
+              <tr key={product.id}>
+                <td>{product.name}</td>
+                <td>{product.sku}</td>
+                <td>{product.advertiser_id}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Table;
