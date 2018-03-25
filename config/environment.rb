219282b8c2b903a76226ccdf35e477e3f86bebf5
Justine Winnie <justine.winnie@gettyimages.com>
2018-03-25 15:35:42 -0700

    Add lib directory to fix pathing issue

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+  config.autoload_paths << "#{Rails.root}/lib"
+  config.autoload_paths << "#{Rails.root}/test/lib"
+end
